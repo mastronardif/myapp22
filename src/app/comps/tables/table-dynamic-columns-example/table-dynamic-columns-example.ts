226e1c17efa5624256ab16fc8063cc93b0bc30d9
Frank Mastronardi <mastronardif@gmail.com>
2023-01-31 12:50:21 -0500

    Cleanup

--- src/app/comps/tables/table-dynamic-columns-example/table-dynamic-columns-example.ts
+++ src/app/comps/tables/table-dynamic-columns-example/table-dynamic-columns-example.ts
@@ -15,8 +15,8 @@ import { HeroService } from 'src/app/services/hero.service';
 export class TableDynamicColumnsExampleComponent implements OnInit {
   showCols = new FormControl('');
 
-  displayedColumns: string[] = [];//['name', 'weight', 'symbol', 'position'];
-  displayedColumnsExtra: string[] = ['edit', 'bobo'];
+  allColumns: string[] = [];
+  displayedColumns: string[] = [];
   columnsToDisplay: string[] = this.displayedColumns.slice();
   users: any[] =[];
   dataSource!: MatTableDataSource<any>;
@@ -32,7 +32,9 @@ export class TableDynamicColumnsExampleComponent implements OnInit {
         next: (v) => {console.log(v);
           this.users = v;
           this.displayedColumns = Object.keys(this.users[0]);
+
           this.columnsToDisplay = this.displayedColumns.slice();
+          this.allColumns = this.displayedColumns.slice();
           //this.displayedColumnsExtra.push('action');
           //this.displayedColumnsAll = this.displayedColumns.concat(this.displayedColumnsExtra);
           this.dataSource = new MatTableDataSource(v);
@@ -40,32 +42,34 @@ export class TableDynamicColumnsExampleComponent implements OnInit {
         },
         error: (e) => console.error(`\tERROR occured: ${JSON.stringify(e)}`),
         complete: () => console.info('complete'),
-        //console.log(this.users);
       });
-    // (users: User[]) => {
-    // this.users = users;
-    // this.dataSource = new MatTableDataSource(users);
-    // this.dataSource.sort = this.sort;
-    //});
   }
-
 
   listColumn(event: any) {
     console.log(event)
+    const cols: string[] = event;
     // console.table(this.displayedColumns);
-    // console.table(this.columnsToDisplay);
+    console.table(this.columnsToDisplay);
+    //this.columnsToDisplay= this.columnsToDisplay.filter(element => !cols.includes(element));
+    this.columnsToDisplay= this.allColumns.filter(element => !cols.includes(element));
+    //console.log(ccc)
+    //const updatedArray = removeElements(array, 'Value', this.displayedColumns);
+    // const updatedArray =this.columnsToDisplay.filter((element) => {
+    //   return element !== '';
+    // });
+
   }
 
-  addColumn() {
-    const randomColumn = Math.floor(Math.random() * this.displayedColumns.length);
-    this.columnsToDisplay.push(this.displayedColumns[randomColumn]);
-  }
+  // addColumn() {
+  //   const randomColumn = Math.floor(Math.random() * this.displayedColumns.length);
+  //   this.columnsToDisplay.push(this.displayedColumns[randomColumn]);
+  // }
 
-  removeColumn() {
-    if (this.columnsToDisplay.length) {
-      this.columnsToDisplay.pop();
-    }
-  }
+  // removeColumn() {
+  //   if (this.columnsToDisplay.length) {
+  //     this.columnsToDisplay.pop();
+  //   }
+  // }
 
   shuffle() {
     let currentIndex = this.columnsToDisplay.length;
